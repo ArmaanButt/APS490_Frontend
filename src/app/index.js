e242d415a23476b39282e43ea728c9637672cad1
Harsh Verma <harsh376@gmail.com>
2017-01-21 16:47:33 -0500

    - configured react, babel, and webpack

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,9 @@
-console.log('hey there');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import 'index.html';
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,51 @@
 const path = require('path');
 const webpack = require('webpack');
 
+const sourcePath = path.join(__dirname, './src/app');
+const destPath = path.join(__dirname, './dist/app');
+
+const rules = [
+  {
+    test: /\.html$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "file-loader",
+      query: {
+        name: "[name].[ext]"
+      }
+    },
+  },
+  {
+    test: /\.(js|jsx)$/,
+    exclude: /node_modules/,
+    use: [
+      'babel-loader',
+    ],
+  },
+];
+
+
 module.exports = {
-  context: path.resolve(__dirname, './src/app'),
+  context: sourcePath,
   entry: {
-    app: './index.js',
+    js: './index.js',
   },
   output: {
-    path: path.resolve(__dirname, './dist/app'),
-    publicPath: '/assets',
-    filename: 'bundle.js',
+    path: destPath,
+    filename: '[name].bundle.js',
+  },
+  module: {
+    rules,
+  },
+  resolve: {
+    extensions: ['.webpack-loader.js', '.web-loader.js', '.loader.js', '.js', '.jsx'],
+    modules: [
+      path.resolve(__dirname, 'node_modules'),
+      sourcePath,
+    ]
+  },
+  devServer: {
+    contentBase: destPath,
+    port: 3000,
   },
 };
